9566aada3379aabbd0ff0bf0c61301c2bc9fd9c6
kawan97 <pshtiwankawan@gmail.com>
2022-09-16 15:15:14 -0700

    add everypixel and now it work

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const multer = require('multer');
 var fs = require('fs');
+const EveryPixel = require('everypixel');
+const api = new EveryPixel({
+	"username": "IkSJx3qJRPx51EvlQTCcetlH",
+	"password": "7oexd27XzHB69HwcVuwixTE47nbQ37nR43TXc2ZaxQBu3jqt"
+});
+
 
 /*------------------------------------------
 --------------------------------------------
@@ -31,15 +37,29 @@ var upload = multer({ storage: storage });
  *
  * @return response()
  */
-//  app.post('/api/image-upload', upload.single('image'),(req, res) => {
 
-app.post('/api/image-upload',upload.single('image'),(req, res) => {
+// app.use(express.static('uploads')); 
+app.use('/uploads', express.static('uploads'));
+
+app.post('/api/image-upload',upload.single('image'),async(req, res) => {
     
     const image = req.image;
-    res.send(apiResponse({message: 'File uploaded successfully.', path:req.file.path}));
-   //delete the photo
+    //get file path
     var filePath =req.file.path; 
-    fs.unlinkSync(filePath);
+    //get host name
+    var hostname = req.headers.host;
+   //create url for image
+   varimageUrl=hostname+"\\"+filePath;
+   
+
+    let requesttoeverypixel = await api.keywords({"url": "https://library.sportingnews.com/styles/twitter_card_120x120/s3/2022-04/Messi%20PSG%20Training.png?itok=yW1geQa6", "num_keywords": 10});
+    
+    Promise.all([requesttoeverypixel]).then((values) => {
+       
+       fs.unlinkSync(filePath);
+       res.send(apiResponse({message: 'File uploaded successfully.', path:varimageUrl,tage:values[0].data}));
+      });
+   //delete the photo
 });
   
 /**
